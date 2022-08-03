c2cda06e80d5b2a340ed9fb1150d762835e91da3
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-08-03 13:31:21 +0200

    Printing out information based on the weather API

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const axios = require('axios');
 
 const API_KEY = '8cad63c32ef56ca20cf8e01f8506b018';
 
-const WEATHER_URL = `http://api.weatherstack.com/current?access_key=${API_KEY}`;
+const WEATHER_URL = `http://api.weatherstack.com/forecast?access_key=${API_KEY}`;
 
 axios({ method: 'get', url: `${WEATHER_URL}&query="52.237049,21.017532"`})
-    .then(response => {const data = response.data; console.log(data.current)})
+    .then(response => {
+        const data = response.data;
+        
+        console.log(`${data.current.weather_descriptions}.\n\nIt is currenly ${data.current.temperature} dagrees out, but it feels like ${data.current.feelslike} dagrees today in ${data.location.name}, ${data.location.country}.`);
+    })
     .catch(err => console.log(err));
