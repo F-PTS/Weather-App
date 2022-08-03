9aa46e035c85f4e160de207efdceaa83d45e13c2
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-08-03 17:45:46 +0200

    adding geocode API

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const axios = require('axios');
 
-const API_KEY = '8cad63c32ef56ca20cf8e01f8506b018';
+const WEATHER_API_KEY = '8cad63c32ef56ca20cf8e01f8506b018';
+const WEATHER_URL = `http://api.weatherstack.com/forecast?access_key=${WEATHER_API_KEY}`;
 
-const WEATHER_URL = `http://api.weatherstack.com/forecast?access_key=${API_KEY}`;
+const GEO_API_KEY = 'pk.eyJ1IjoicGl0dXNzIiwiYSI6ImNsNmRxeXY3cjE0djczYnFsb256NHRuaDMifQ.HQg_9QkXobu7kEqDG_f1bg';
 
-axios({ method: 'get', url: `${WEATHER_URL}&query="52.237049,21.017532"`})
+const getWeather = (latitude, longitude) => {
+    axios({ method: 'get', url: `${WEATHER_URL}&query="${latitude},${longitude}"`})
     .then(response => {
         const data = response.data;
-        
+
         console.log(`${data.current.weather_descriptions}.\n\nIt is currenly ${data.current.temperature} dagrees out, but it feels like ${data.current.feelslike} dagrees today in ${data.location.name}, ${data.location.country}.`);
     })
     .catch(err => console.log(err));
+};
+
+const getGeoCode = place => {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(place)}.json?access_token=${GEO_API_KEY}`;
+
+    axios({method: 'get', url})
+        .then(response => {
+            const latitude = response.data.features[0].center[1];
+            const longitude = response.data.features[0].center[0];
+
+            getWeather(latitude, longitude);
+        })
+        .catch(err => console.log(err));
+}
+
+getGeoCode('Slupsk');
