6bef452173ff4c01b6b473a1950b32adc84467ee
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-08-04 01:37:56 +0200

    adding things

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.set('view engine', 'hbs');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-const viewsPath = path.join(__dirname, '../templates');
+app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -36,6 +40,10 @@ app.get('/weather', (req, res) => {
         location: 'Poland'
     })
 });
+
+app.get('*', (req, res) => {
+    res.send("404! The page doesn't exist")
+})
 
 app.listen(3000, () => {
     console.log(`server is up on port 3000.`);
