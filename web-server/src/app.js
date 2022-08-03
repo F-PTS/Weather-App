abcdbc4398cb04e6dec42a90f275ca791f0dc2d9
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-08-04 00:06:59 +0200

    adding dynamic templates with handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,40 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello express');
-});
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'hbs');
 
-app.get('/help', (req, res) => {
-    res.send('Help page');
+const viewsPath = path.join(__dirname, '../templates');
+app.set('views', viewsPath);
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Lorem ipsum dolor sit amet.'
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.send('This is my page')
+    res.render('about', {
+        title: 'About us title',
+        name: 'Lorem ipsum dolor sit amet consectetur.'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help title',
+        name: 'Lorem ipsum dolor sit amet consectetur.'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send('weather nice today indeed yes very yes')
+    res.send({
+        forecast: 'It is sunny',
+        location: 'Poland'
+    })
 });
 
 app.listen(3000, () => {
