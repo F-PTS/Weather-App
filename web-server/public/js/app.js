68702b73ab2aac04a6e6c4d1734eb7128eff4f0c
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-08-04 16:15:53 +0200

    linking frontend to backend api

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,19 +1,19 @@
-fetch('http://localhost:3000/weather?adress=Sławno')
-    .then(res => {
-        res.json().then(data => {
-            if(data.error) {
-                console.log(data.error);
-            } else {
-                console.log(data.temperature);
-                console.log(data.location);
-            }
-        })
-    }).catch(err => {console.log(err)})
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input')
+const result = document.querySelector('.result');
 
 weatherForm.addEventListener('submit', e => {
     e.preventDefault();
     console.log(search.value)
+
+    fetch(`http://localhost:3000/weather?adress=${search.value}`)
+    .then(res => {
+        res.json().then(data => {
+            if(data.error) {
+                result.textContent=`Error! \n${data.error}`
+            } else {
+                result.textContent=`It is ${data.forecast} right now, and it is ${data.temperature} dagrees out. Feels like ${data.feelsLike}. All in ${data.location},${data.city},${data.country}`
+            }
+        })
+    }).catch(err => {console.log(err)})
 })
